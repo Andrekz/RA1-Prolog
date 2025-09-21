c9c439d4c79df12d3f33769aefca34799822c00f
Andrekz <andrevinisou@gmail.com>
2025-09-21 14:01:37 -0300

    Add files via upload

---
+++ perfil_ia.pl
@@ -0,0 +1,13 @@
+%Perfil focado em Inteligência Artificial
+
+resposta(1, s).  
+resposta(2, s).  
+resposta(3, n).  
+resposta(4, n).  
+resposta(5, n).  
+resposta(6, n).  
+resposta(7, n). 
+resposta(8, n).  
+resposta(9, n).  
+resposta(10, n). 
+resposta(11, n). 
