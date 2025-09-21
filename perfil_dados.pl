75991f75d5818d0a977d5bb00028d109813495f1
Andrekz <andrevinisou@gmail.com>
2025-09-21 18:34:08 -0300

    Add files via upload

---
+++ perfil_dados.pl
@@ -0,0 +1,12 @@
+% Perfil Engenharia de dados
+resposta(1, n).  
+resposta(2, s).  
+resposta(3, n).  
+resposta(4, n).  
+resposta(5, n).
+resposta(6, s).  
+resposta(7, s). 
+resposta(8, n).  
+resposta(9, n). 
+resposta(10, n).
+resposta(11, n). 
