2d5c5164e4fe3f2a8d2628c1152cb163dd5c925a
Andrekz <andrevinisou@gmail.com>
2025-09-22 09:47:43 -0300

    Update and rename perfil_web.pl to perfil_teste3.pl

---
+++ perfil_teste3.pl
@@ -0,0 +1,14 @@
+
+%perfil focado dem desenvolvimento web
+ resposta(1, n). 
+ resposta(2, s).  
+ resposta(3, s). 
+ resposta(4, n).  
+ resposta(5, n). 
+ resposta(6, n). 
+ resposta(7, n). 
+ resposta(8, n).  
+ resposta(9, n).  
+ resposta(10, n).
+ resposta(11, s). 
+
